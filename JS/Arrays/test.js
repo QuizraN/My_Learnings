f3f6bf9047f000e4066fb329b799b5a3d3916046
Quizra <quizra@beautifulcode.in>
2022-07-15 13:08:07 +0530

    Arrays folder added

---
+++ JS/Arrays/test.js
@@ -0,0 +1,7 @@
+let a=[1,2,3]
+function f1(x)
+{
+x[1]=6;
+return x;
+}
+console.log(f1(a));
---
+++ JS/Arrays/test2.js
@@ -0,0 +1,4 @@
+let a=[1,2,3,4,5]
+console.log(a.slice())
+console.log(Array.from(a))
+console.log([...a])
