e573a61b7cc866e03c02af5d06571925e3779987
Quizra <quizra.16@gmail.com>
2022-10-19 10:23:41 +0530

    iterators exercise 1 added

---
+++ Python/exercises/iterators.py
@@ -0,0 +1,54 @@
+import time
+
+class custom_range:
+    def __init__(self,start,stop,step=1):
+        self.start=start
+        self.stop=stop
+        self.step=step
+        self.count=start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if(self.count<self.stop):
+            value=self.count
+            self.count += self.step
+            return value
+        else:
+            raise StopIteration()
+
+
+def custom_range2(start=0,stop=0,step=1):
+    count=start
+    result=[]
+    while count!=stop:
+        result.append(count)
+        count+=1
+    
+    iterable=iter(result)
+    return iterable
+
+if __name__=='__main__':
+
+    start_time = time.time()
+    for i in range(2,5):
+        print(i)
+    # print(range(0))
+    print("range function took", time.time() - start_time, "to run")
+
+    start_time = time.time()
+    for i in custom_range(2,5):
+        print(i)
+    # print(range(0))
+    print("custom_range function took", time.time() - start_time, "to run")
+   
+    start_time = time.time()
+    for i in custom_range2(2,5):
+        print(i)
+    # print(range(0))
+    print("custom_range function took", time.time() - start_time, "to run")
+
+
+
+
